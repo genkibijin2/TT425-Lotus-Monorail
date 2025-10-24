10ffe88402e9e6d1f72761c193c4e2b5d6d5c60e
genkibijin2 <Kendall.Price@Euroglaze.Limited>
2025-10-24 13:56:17 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace TT425_Lotus_Monorail
+{
+    public partial class mainWindow : Form
+    {
+        public mainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
