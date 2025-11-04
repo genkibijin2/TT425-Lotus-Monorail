05c87e6b7a847264046d47f48769823957c5dad9
genkibijin2 <Kendall.Price@Euroglaze.Limited>
2025-11-04 11:33:23 +0000

    //Update for Backup//

--- mainWindow.Designer.cs
+++ mainWindow.Designer.cs
@@ -23,23 +23,204 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainWindow));
+            folderSelector = new FolderBrowserDialog();
+            folderLocationBox = new TextBox();
+            logBox = new RichTextBox();
+            removableDrivesSelection = new ComboBox();
+            statusLight = new PictureBox();
+            fileCheckerStatusText = new RichTextBox();
+            USBCheckLight = new PictureBox();
+            USBStatusText = new RichTextBox();
+            RefreshButton = new PictureBox();
+            changeFolderIcon = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)statusLight).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)USBCheckLight).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)RefreshButton).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)changeFolderIcon).BeginInit();
             SuspendLayout();
+            // 
+            // folderSelector
+            // 
+            folderSelector.Description = "Folder With Bar Optimised Files";
+            folderSelector.RootFolder = Environment.SpecialFolder.MyComputer;
+            folderSelector.SelectedPath = "C:\\sawfiles_two";
+            folderSelector.UseDescriptionForTitle = true;
+            // 
+            // folderLocationBox
+            // 
+            folderLocationBox.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            folderLocationBox.BorderStyle = BorderStyle.FixedSingle;
+            folderLocationBox.Cursor = Cursors.Hand;
+            folderLocationBox.Font = new Font("SimSun", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            folderLocationBox.Location = new Point(12, 26);
+            folderLocationBox.Margin = new Padding(9, 6, 9, 6);
+            folderLocationBox.MaximumSize = new Size(378, 26);
+            folderLocationBox.Name = "folderLocationBox";
+            folderLocationBox.ReadOnly = true;
+            folderLocationBox.Size = new Size(378, 26);
+            folderLocationBox.TabIndex = 1;
+            folderLocationBox.Text = "C:\\Sawfiles_two";
+            folderLocationBox.MouseClick += locationBoxClick;
+            // 
+            // logBox
+            // 
+            logBox.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            logBox.BackColor = Color.DarkSlateBlue;
+            logBox.BorderStyle = BorderStyle.FixedSingle;
+            logBox.Font = new Font("SimSun", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            logBox.ForeColor = Color.MintCream;
+            logBox.Location = new Point(12, 216);
+            logBox.MaximumSize = new Size(378, 109);
+            logBox.MinimumSize = new Size(378, 109);
+            logBox.Name = "logBox";
+            logBox.ReadOnly = true;
+            logBox.ScrollBars = RichTextBoxScrollBars.ForcedVertical;
+            logBox.Size = new Size(378, 109);
+            logBox.TabIndex = 3;
+            logBox.Text = "Loading...";
+            // 
+            // removableDrivesSelection
+            // 
+            removableDrivesSelection.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            removableDrivesSelection.DropDownStyle = ComboBoxStyle.DropDownList;
+            removableDrivesSelection.Font = new Font("SimSun", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            removableDrivesSelection.FormattingEnabled = true;
+            removableDrivesSelection.Location = new Point(12, 93);
+            removableDrivesSelection.MaximumSize = new Size(378, 0);
+            removableDrivesSelection.MinimumSize = new Size(378, 0);
+            removableDrivesSelection.Name = "removableDrivesSelection";
+            removableDrivesSelection.Size = new Size(378, 24);
+            removableDrivesSelection.TabIndex = 4;
+            removableDrivesSelection.SelectedIndexChanged += removableDrivesSelection_SelectedIndexChanged;
+            // 
+            // statusLight
+            // 
+            statusLight.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            statusLight.Image = Properties.Resources.FileCheckNG;
+            statusLight.Location = new Point(12, 61);
+            statusLight.Name = "statusLight";
+            statusLight.Size = new Size(26, 26);
+            statusLight.TabIndex = 6;
+            statusLight.TabStop = false;
+            // 
+            // fileCheckerStatusText
+            // 
+            fileCheckerStatusText.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            fileCheckerStatusText.BackColor = Color.Black;
+            fileCheckerStatusText.BorderStyle = BorderStyle.FixedSingle;
+            fileCheckerStatusText.Font = new Font("SimSun", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            fileCheckerStatusText.ForeColor = Color.White;
+            fileCheckerStatusText.Location = new Point(43, 61);
+            fileCheckerStatusText.Multiline = false;
+            fileCheckerStatusText.Name = "fileCheckerStatusText";
+            fileCheckerStatusText.ReadOnly = true;
+            fileCheckerStatusText.ScrollBars = RichTextBoxScrollBars.None;
+            fileCheckerStatusText.Size = new Size(347, 26);
+            fileCheckerStatusText.TabIndex = 7;
+            fileCheckerStatusText.Text = "Checking folder for saw files...";
+            fileCheckerStatusText.WordWrap = false;
+            // 
+            // USBCheckLight
+            // 
+            USBCheckLight.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            USBCheckLight.Image = Properties.Resources.FileCheckNG;
+            USBCheckLight.Location = new Point(12, 123);
+            USBCheckLight.Name = "USBCheckLight";
+            USBCheckLight.Size = new Size(26, 26);
+            USBCheckLight.TabIndex = 8;
+            USBCheckLight.TabStop = false;
+            // 
+            // USBStatusText
+            // 
+            USBStatusText.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            USBStatusText.BackColor = Color.Black;
+            USBStatusText.BorderStyle = BorderStyle.FixedSingle;
+            USBStatusText.Font = new Font("SimSun", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            USBStatusText.ForeColor = Color.White;
+            USBStatusText.Location = new Point(43, 124);
+            USBStatusText.Multiline = false;
+            USBStatusText.Name = "USBStatusText";
+            USBStatusText.ReadOnly = true;
+            USBStatusText.ScrollBars = RichTextBoxScrollBars.None;
+            USBStatusText.Size = new Size(302, 26);
+            USBStatusText.TabIndex = 9;
+            USBStatusText.Text = "Checking USB...";
+            USBStatusText.WordWrap = false;
+            // 
+            // RefreshButton
+            // 
+            RefreshButton.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            RefreshButton.Image = Properties.Resources.RefreshUsbIcon;
+            RefreshButton.Location = new Point(349, 121);
+            RefreshButton.Name = "RefreshButton";
+            RefreshButton.Size = new Size(39, 30);
+            RefreshButton.TabIndex = 10;
+            RefreshButton.TabStop = false;
+            RefreshButton.Click += RefreshButton_Click;
+            RefreshButton.MouseEnter += RefreshButton_MouseEnter;
+            RefreshButton.MouseLeave += RefreshButton_MouseLeave;
+            // 
+            // changeFolderIcon
+            // 
+            changeFolderIcon.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            changeFolderIcon.Image = Properties.Resources.ChangeFolderIcon;
+            changeFolderIcon.Location = new Point(12, 6);
+            changeFolderIcon.Name = "changeFolderIcon";
+            changeFolderIcon.Size = new Size(25, 20);
+            changeFolderIcon.TabIndex = 11;
+            changeFolderIcon.TabStop = false;
+            changeFolderIcon.Click += changeFolderIcon_Click;
+            changeFolderIcon.MouseEnter += changeFolderIcon_MouseEnter;
+            changeFolderIcon.MouseLeave += changeFolderIcon_MouseLeave;
             // 
             // mainWindow
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(12F, 21F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(530, 407);
+            ClientSize = new Size(408, 337);
+            Controls.Add(changeFolderIcon);
+            Controls.Add(RefreshButton);
+            Controls.Add(USBStatusText);
+            Controls.Add(USBCheckLight);
+            Controls.Add(fileCheckerStatusText);
+            Controls.Add(statusLight);
+            Controls.Add(removableDrivesSelection);
+            Controls.Add(logBox);
+            Controls.Add(folderLocationBox);
+            Font = new Font("MS PMincho", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(5, 4, 5, 4);
+            MaximumSize = new Size(416, 363);
+            MinimumSize = new Size(416, 363);
             Name = "mainWindow";
             Text = "Lotus Monorail";
+            Click += mainWindow_Click;
+            ((System.ComponentModel.ISupportInitialize)statusLight).EndInit();
+            ((System.ComponentModel.ISupportInitialize)USBCheckLight).EndInit();
+            ((System.ComponentModel.ISupportInitialize)RefreshButton).EndInit();
+            ((System.ComponentModel.ISupportInitialize)changeFolderIcon).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
+        private System.Windows.Forms.TextBox folderLocationBox;
+
+        private System.Windows.Forms.FolderBrowserDialog folderSelector;
+
         #endregion
+        private RichTextBox logBox;
+        private ComboBox removableDrivesSelection;
+        private PictureBox statusLight;
+        private RichTextBox fileCheckerStatusText;
+        private PictureBox USBCheckLight;
+        private RichTextBox USBStatusText;
+        private PictureBox RefreshButton;
+        private PictureBox changeFolderIcon;
     }
 }
--- mainWindow.cs
+++ mainWindow.cs
@@ -1,10 +1,338 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
 namespace TT425_Lotus_Monorail
 {
     public partial class mainWindow : Form
     {
+        /*===GLOBAL VARIABLES===*/
+        //####
+        String defaultDirectory = @"C:\sawfiles_test"; //default directory for .prt files, change to C:\sawfiles_two when live
+        string currentSelectedUSBDrive = ""; //global that holds the current USB drive letter/path, starts blank
+        //####
+
+        /*===BOOLEANS AND ERROR CHECKERS===*/
+        //#
+        Boolean removableDrivesAvailable = false; //Becomes true if USB/Removable Drives available
+        Boolean sawFolderExists = false; //Becomes true if the default directory exists.
+        Boolean sawFolderFilesArePRT = false;   //Becomes true if there are .prt files in the saw files folder
+        Boolean USBComparisonSuccessful = false; //becomes true if USB has been checked and there are files ready to write
+        Boolean readyToStartWriting = false; //Becomes true if it is okay to begin writing files
+        Boolean sawFileNotYetWritten = false;   //Becomes true if the saw file does not also exist on target removable drive
+        Boolean extremeErrorState = false; //Becomes true if an exception is thrown that should stop the program from continuing
+        //#
+
+
         public mainWindow()
+        //###
+        //Main Window and Startup process
         {
+            //Initialization/Runtime Commands
             InitializeComponent();
+            //Startup Processes
+            printToLog("loaded!");
+            folderLocationBox.Text = defaultDirectory;
+
+            //Drive Check
+            removableDrivesSelection.Items.Add("No USB Drives Found");
+            checkRemovableDrives();
+
+            //Try to create default directory (C:\sawfiles_two) if doesnt exist
+            checkIfDefaultDirectoryExists();
+
+            //Check default location for files
+            if (sawFolderExists)
+            //Continue if default folder exists, which it should even if it has to be created.
+            {
+                checkForPRTFiles(defaultDirectory); //Check default directory for PRT Files
+                CheckUSBProcess();
+            }
+
+
         }
+        //###
+
+        //Folder Picker Methods//
+        private void locationBoxClick(object sender, MouseEventArgs e)
+        {
+            changeFolderLocation();
+        }
+        private void changeFolderIcon_Click(object sender, EventArgs e)
+        {
+            changeFolderLocation();
+        }
+        //--------------------//
+
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            checkRemovableDrives();
+        }
+
+        private void removableDrivesSelection_SelectedIndexChanged(object sender, EventArgs e)
+        //If dropdown changed, only perform a check if there's removable drives accessible
+        {
+            CheckUSBProcess();
+        }
+
+        private void changeFolderLocation()
+        {
+            //Method for picking a file, cut and paste to whichever button you
+            //end up using to select this...
+            DialogResult location2StartResult = folderSelector.ShowDialog();
+            if (location2StartResult == DialogResult.OK)
+            {
+
+                string chosenStartFolder = folderSelector.SelectedPath;
+                folderLocationBox.Text = chosenStartFolder;
+                checkForPRTFiles(chosenStartFolder);
+                CheckUSBProcess();
+            }
+        }
+
+        private void CheckUSBProcess()
+        {
+            if (removableDrivesAvailable && sawFolderFilesArePRT)
+            {
+                string justTheDriveLetterPlease = ""; //initialise string
+                string? selectedDriveFullName = ""; //initialise non null string
+                string chosenStartFolder = folderSelector.SelectedPath;
+                //Method here to grab Drive Letter From Selection
+                selectedDriveFullName = removableDrivesSelection.GetItemText(removableDrivesSelection.SelectedItem);
+                if (selectedDriveFullName != null)
+                { //Makes sure there's actual information in the name, and isn't null
+                    justTheDriveLetterPlease = selectedDriveFullName.Substring(0, 3);
+                }
+                string USBDriveToChangeTo = justTheDriveLetterPlease;
+                currentSelectedUSBDrive = USBDriveToChangeTo;
+                compareFolderFilesToUSB(chosenStartFolder, USBDriveToChangeTo);
+
+            }
+            else if (removableDrivesAvailable && !sawFolderFilesArePRT)
+            //If USB detected but no .prt files in folder
+            {
+                USBStatusText.Text = $"No .prt files to write";
+                USBCheckLight.Image = Properties.Resources.FileCheckNG;
+            }
+            else
+            //If removable drives not available
+            {
+                USBStatusText.Text = $"No USB";
+                USBCheckLight.Image = Properties.Resources.FileCheckNG;
+                //CHANGE THIS to code to reset the PRT comparison box
+            }
+        }
+
+        private void printToLog(string text2Print)
+        //Appends log text to the top of the log box
+        {
+
+
+            DateTime currentDate = DateTime.Now;
+            String justTheTime = currentDate.ToString("HH:mm:ss");
+            //Check Size of log, will wipe when very very long
+            if (logBox.Text.Length > 200000)
+            {
+                logBox.Text = ("[" + justTheTime + "]" + " Log cleared");
+            }
+            logBox.Text = ("[" + justTheTime + "] " + text2Print + Environment.NewLine + logBox.Text);
+        }
+
+        private void checkRemovableDrives()
+        //Checks if there is available USB or Removable disks
+        {
+            removableDrivesSelection.Items.Clear();
+            removableDrivesAvailable = false; //RESET FLAG
+            int driveIndex = 0;
+            printToLog("Checking for Drives...");
+            try
+            {
+                DriveInfo[] allSystemDrives = DriveInfo.GetDrives();
+                foreach (DriveInfo currentItem in allSystemDrives)
+                {
+
+                    if (currentItem.DriveType == DriveType.Removable)
+                    {
+                        driveIndex++; //Count new drive
+                        removableDrivesAvailable = true;
+                        //Calculate size//
+                        decimal driveSizeInBytes = currentItem.TotalSize;
+                        String driveSizeFinalString = "";
+                        if (driveSizeInBytes < 1000000000)
+                        {
+                            //Under 1GB so convert to a MB reading
+                            driveSizeInBytes = (driveSizeInBytes / 1000000); //MB Value
+                            driveSizeInBytes = Math.Round(driveSizeInBytes, 0);
+                            driveSizeFinalString = (driveSizeInBytes + "MB");
+                        }
+                        else if (driveSizeInBytes < 1000000)
+                        {
+                            //Under 1MB so convert to a KB reading
+                            driveSizeInBytes = (driveSizeInBytes / 1000); //MB Value
+                            driveSizeInBytes = Math.Round(driveSizeInBytes, 0);
+                            driveSizeFinalString = (driveSizeInBytes + "KB");
+                        }
+                        else
+                        {
+                            //Over 1GB so convert to GB reading
+                            driveSizeInBytes = (driveSizeInBytes / 1000000000); //GB value
+                            driveSizeInBytes = Math.Round(driveSizeInBytes, 2);
+                            driveSizeFinalString = (driveSizeInBytes + "GB");
+                        }
+                        //Size Caluclation Complete
+                        //Volume Label//
+                        String volumeLabelName = currentItem.VolumeLabel;
+                        //Check for blank name
+                        if (volumeLabelName == "")
+                        {
+                            volumeLabelName = "Unnamed Drive";
+                        }
+
+                        String foundDriveInfo = (currentItem.Name + " " +
+                                                "[" + volumeLabelName + "] " + driveSizeFinalString);
+                        printToLog("Found Drive: " + foundDriveInfo);
+                        removableDrivesSelection.Items.Add(foundDriveInfo);
+                        if (driveIndex == 1)
+                        {
+                            //If first USB found, set as default path, as this will also be the dropdown default selection.
+                            currentSelectedUSBDrive = currentItem.Name;
+                            printToLog($"Setting default USB path as {currentSelectedUSBDrive}");
+                        }
+                    }
+
+                }
+                //Change dropdown Default to first USB
+            }//END OF TRY LOOP
+            catch (Exception ex)
+            //If there was a IO error with checking drives
+            {
+                printToLog("Drive Check Error: " + ex.Message);
+                removableDrivesAvailable = false;
+            }
+            //END OF DRIVE CHECK LOOP
+            if ((removableDrivesAvailable == false) | (removableDrivesSelection.Items.Count == 0))
+            //If no removable drives found
+            {
+                printToLog("No USB Drives found!");
+                removableDrivesSelection.Items.Add("No USB Drives Found");
+
+            }
+            if (removableDrivesAvailable == true)
+            {
+                removableDrivesSelection.SelectedIndex = 0;
+            }
+            else
+            {
+                removableDrivesSelection.SelectedIndex = removableDrivesSelection.Items.Count - 1;
+            }
+            printToLog("Drive Check Complete");
+            //FLAG RESET
+        }
+
+        private void checkIfDefaultDirectoryExists()
+        //Checks for the existence of C:\sawfiles_two, creates location if not
+        {
+            printToLog("Checking if " + defaultDirectory + " exists...");
+            Boolean doesDefaultExist = System.IO.Directory.Exists(defaultDirectory);
+            if (doesDefaultExist)
+            {
+                printToLog($"{defaultDirectory} found");
+                sawFolderExists = true;
+            }
+            else
+            {
+                printToLog($"{defaultDirectory} not found");
+                printToLog($"Creating {defaultDirectory}");
+                try
+                {
+                    System.IO.Directory.CreateDirectory(defaultDirectory);
+                    sawFolderExists = true;
+                }
+                catch (Exception ex)
+                {
+                    printToLog($"Error creating {defaultDirectory}: " + ex.Message);
+                    fileCheckerStatusText.Text = ($"Error creating {defaultDirectory}");
+                    sawFolderExists = false;
+                }
+            }
+        }
+
+        private void checkForPRTFiles(String directoryToScan)
+        {
+            printToLog($"Checking {directoryToScan} for .prt files...");
+            string[] fileNames = Directory.GetFiles(directoryToScan, "*.prt", SearchOption.TopDirectoryOnly);
+            if (fileNames.Length != 0)
+            //Files Found
+            {
+                int numberOfPRTFilesFound = 0;
+                foreach (string fileName in fileNames)
+                {
+                    numberOfPRTFilesFound++;
+                    printToLog($"Found {fileName}");
+                }
+                statusLight.Image = Properties.Resources.FileCheckOk;
+                fileCheckerStatusText.Text = $"Found {numberOfPRTFilesFound} .prt files in folder";
+                sawFolderFilesArePRT = true;
+            }
+            else
+            //File Not Found
+            {
+                sawFolderFilesArePRT = false;
+                fileCheckerStatusText.Text = $"No .prt files found in folder";
+                statusLight.Image = Properties.Resources.FileCheckNG;
+                printToLog($"No .prt files found in {directoryToScan}");
+                sawFolderFilesArePRT = false;
+
+            }
+        }
+
+        private void compareFolderFilesToUSB(string locationToCheck, string USBToCheck)
+        //This method is only reached when a USB is available and .prt files are found in the chosen folder
+        //So, don't worry about not having access to drive names or folder paths - They are already checked
+        {
+            USBComparisonSuccessful = false; //RESET FLAG
+            printToLog($"Comparing contents of {locationToCheck} to USB Drive {USBToCheck}");
+
+
+
+
+            //Put this flag change wherever the comparison is successful...
+            USBComparisonSuccessful = true;
+            if (USBComparisonSuccessful)
+            {
+                //SUCCESS STATE - WRAP WITH SUCCESS FLAG AFTER FINISHING
+                USBStatusText.Text = $"Ready To Write";
+                USBCheckLight.Image = Properties.Resources.FileCheckOk;
+                //-----------------------------------------------------------------------------------//
+            }
+        }
+
+
+
+
+
+
+
+
+        //--------------------------GENERAL UI, ANIMATIONS, HOVER CHANGES ETC-------------------------//
+        private void RefreshButton_MouseEnter(object sender, EventArgs e)
+        {
+            RefreshButton.Image = Properties.Resources.RefreshUsbIconHover;
+        }
+        private void RefreshButton_MouseLeave(object sender, EventArgs e)
+        {
+            RefreshButton.Image = Properties.Resources.RefreshUsbIcon;
+        }
+        private void mainWindow_Click(object sender, EventArgs e)
+        {
+        }
+        private void changeFolderIcon_MouseEnter(object sender, EventArgs e)
+        {
+            changeFolderIcon.Image = Properties.Resources.ChangeFolderIconHover;
+        }
+        private void changeFolderIcon_MouseLeave(object sender, EventArgs e)
+        {
+            changeFolderIcon.Image = Properties.Resources.ChangeFolderIcon;
+        }
+        //--------------------------------------------------------------------------------------------//
     }
 }
